bfd9e89b15ab4e20776213f8a88a2d10e883671e
Jonatan Eriksson <58311888+jonatan-eriksson@users.noreply.github.com>
2021-04-21 15:08:59 +0200

    Add post endpoint to enable translation requests

---
+++ RobberLanguageApi/Controllers/TranslationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RobberLanguageApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RobberLanguageApi.Controllers
+{
+    [Route("api/RobberLanguage")]
+    [ApiController]
+    public class TranslationController : ControllerBase
+    {
+        [Route("CreateTranslation")]
+        [HttpPost]
+        public ActionResult<Translation> Translate(Translation translation)
+        {
+            return CreatedAtAction("Get", new Translation(translation.OriginalSentence));
+        }
+    }
+}
