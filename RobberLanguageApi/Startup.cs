162e47cf7c7c21fddae04be67b2d4e4c55c660ae
Jonatan Eriksson <58311888+jonatan-eriksson@users.noreply.github.com>
2021-05-05 09:55:21 +0200

    Add xml documentation to swagger

--- RobberLanguageApi/Startup.cs
+++ RobberLanguageApi/Startup.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+to using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +15,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RobberLanguageApi.ApiKey;
 using RobberLanguageApi.Data;
 
 namespace RobberLanguageApi
@@ -30,12 +34,31 @@ namespace RobberLanguageApi
         {
 
             services.AddControllers();
+
+            services.AddApiVersioning(o => {
+                o.DefaultApiVersion = new ApiVersion(2, 0);
+                o.AssumeDefaultVersionWhenUnspecified = true;
+                o.ReportApiVersions = true;
+            });
+            
+            services.AddVersionedApiExplorer(o =>
+            {
+                o.GroupNameFormat = "'v'VVV";
+                o.SubstituteApiVersionInUrl = true;
+            });
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RobberLanguageApi", Version = "v1" });
+                c.SwaggerDoc("v2", new OpenApiInfo { Title = "RobberLanguageApi", Version = "v2" });
+
+                var xmlDocsPath = Path.Combine(AppContext.BaseDirectory, "Documentation.xml");
+                c.IncludeXmlComments(xmlDocsPath);
             });
+
             services.AddDbContext<RobberTranslationDbContext>(options =>
                 options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Translations"));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,13 +68,18 @@ namespace RobberLanguageApi
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RobberLanguageApi v1"));
+                app.UseSwaggerUI(c =>
+                { 
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "RobberLanguageApi v1");
+                    c.SwaggerEndpoint("/swagger/v2/swagger.json", "RobberLanguageApi v2");
+                });
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
